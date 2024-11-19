5446376a728b7c8c41d540ad7b6f8736437ac655
elleyamamotouw <elleyama@uw.edu>
2024-11-19 10:46:39 -0800

    ignoring keys

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from operator import truediv
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -8,4 +10,13 @@ def index():
             {"name": "oranges", "quantity": 2},
             {"name": "strawberries", "quantity": 6}
     ]
-    return render_template("index.html", fruits=fruits)
+    fruits_o_start = [fruit for fruit in fruits if check_o_start(fruit)]
+    fruits_more_than_3 = [fruit for fruit in fruits if check_more_than_3(fruit)]
+
+    return render_template("index.html", fruits_o_start=fruits_o_start)
+
+def check_o_start(fruit_info):
+    return fruit_info["name"][0].lower() == "o"
+
+def check_more_than_3(fruit_info):
+    return fruit_info["quantity"] > 3
